b4b2af126372d699d7ed2c0c174f23be62f56e7d
ranjian0 <karanjaichungwa@gmail.com>
2018-06-05 14:34:29 +0300

    refactor to single file -- quicker dev

--- triggered/scenes.py
+++ triggered/scenes.py
@@ -4,79 +4,24 @@ import pyglet as pg
 
 from resources import Resources
 from gui import Label, Title, Button
-# from levels    import (
-#     LevelManager, Level, FAILED_EVT, PASSED_EVT)
-
-class Scene:
-
-    def __init__(self, name):
-        self.name     = name
-        self.elements = []
-
-        self.paused = False
-
-    def add(self, items):
-        if isinstance(items, list):
-            self.elements.extend(items)
-        else:
-            self.elements.append(items)
-
-    def draw(self):
-        for elem in self.elements:
-            if hasattr(elem, 'draw'):
-                elem.draw()
-
-    def update(self, dt):
-        if self.paused: return
-        for elem in self.elements:
-            if hasattr(elem, 'update'):
-                elem.update(dt)
-
-    def event(self, ev):
-        if self.paused: return
-        for elem in self.elements:
-            if hasattr(elem, 'event'):
-                elem.event(ev)
 
 class SceneManager:
 
-    # -- singleton
-    instance = None
-    def __new__(cls):
-        if SceneManager.instance is None:
-            SceneManager.instance = object.__new__(cls)
-        return SceneManager.instance
+    # # -- singleton
+    # instance = None
+    # def __new__(cls, *args):
+    #     if SceneManager.instance is None:
+    #         SceneManager.instance = object.__new__(cls)
+    #     return SceneManager.instance
 
-    def __init__(self):
-        self.scenes  = []
-        self.current = None
-
-    def __iter__(self):
-        for scn in self.scenes:
-            yield scn
-
-    def add(self, scene, is_main=False):
-        if is_main:
-            self.current = scene
-        self.scenes.append(scene)
-
-    def switch(self, name):
-        for scn in self.scenes:
-            if scn.name == name:
-                self.current = scn
-                break
-
-    def resume(self):
-        self.current.paused = False
-        self.switch("Game")
+    def __init__(self, window):
+        pass
 
     def draw(self):
-        if self.current:
-            self.current.draw()
+        pass
 
     def update(self, dt):
-        if self.current:
-            self.current.update(dt)
+        pass
 
     def key_press(self, key, modifiers):
         pass
@@ -94,17 +39,34 @@ class SceneManager:
         pass
 
 
-def init_scenes():
-    MainScene = Scene("Main")
-    MainScene.add([
-        pg.text.Label('TRIGGERED',
-                          font_name='Times New Roman',
-                          font_size=40,
-                          x=100, y=100,
-                          anchor_x='center', anchor_y='center'),
-    ])
 
-    return [MainScene]
+def main_scene():
+    vbox = glooey.VBox()
+    vbox.alignment = 'center'
+
+    title = Title("Triggered")
+    vbox.add(title)
+
+    buttons = [
+           Button("Play", lambda : print("Play")),
+           Button("Exit", lambda : print("Exit")),
+    ]
+    for button in buttons:
+       vbox.add(button)
+
+    return vbox
+
+# def init_scenes():
+#     MainScene = Scene("Main")
+#     MainScene.add([
+#         pg.text.Label('TRIGGERED',
+#                           font_name='Times New Roman',
+#                           font_size=40,
+#                           x=100, y=100,
+#                           anchor_x='center', anchor_y='center'),
+#     ])
+
+#     return [MainScene]
 
     # GameScene = Scene("Game")
     # levels = LevelManager()
--- triggered/triggered.py
+++ triggered/triggered.py
@@ -1,10 +1,9 @@
+import os
 import sys
 import pyglet as pg
 from pyglet.gl import *
 
-from resources import Resources
-from scenes import (
-    SceneManager, init_scenes)
+from collections import defaultdict, namedtuple
 
 FPS        = 60
 SIZE       = (800, 600)
@@ -12,16 +11,117 @@ CAPTION    = "Triggered"
 BACKGROUND = (100, 100, 100)
 
 
+'''
+============================================================
+---   CLASSES
+============================================================
+'''
+
+Resource = namedtuple("Resource", "name data")
+
+class Resources:
+
+    # -- singleton
+    instance = None
+    def __new__(cls):
+        if Resources.instance is None:
+            Resources.instance = object.__new__(cls)
+        return Resources.instance
+
+    def __init__(self, root_dir="res"):
+        self.root = root_dir
+        pg.resource.path = [
+            os.path.join(os.path.dirname(os.path.realpath(__file__)), self.root)
+        ]
+        pg.resource.reindex()
+
+        abspath = os.path.abspath
+        self._sprites = abspath(os.path.join(root_dir, "sprites"))
+        self._sounds  = abspath(os.path.join(root_dir, "sounds"))
+        self._levels  = abspath(os.path.join(root_dir, "levels"))
+
+        self._data = defaultdict(list)
+        self._load()
+
+    def sprite(self, name):
+        for res in self._data['sprites']:
+            if res.name == name:
+                return res.data
+        return None
+
+    def sound(self, name):
+        for res in self._data['sounds']:
+            if res.name == name:
+                return res.data
+        return None
+
+    def level(self, name):
+        for res in self._data['levels']:
+            if res.name == name:
+                return res.data
+        return None
+
+    def _load(self):
+
+        # -- load sprites
+        for sprite in os.listdir(self._sprites):
+            img = pg.resource.image('sprites/' + sprite)
+            fn = os.path.basename(sprite.split('.')[0])
+            self._data['sprites'].append(Resource(fn,img))
+
+        # -- load sounds
+        for sound in os.listdir(self._sounds):
+            snd = pg.resource.media('sounds/' + sound)
+            fn = os.path.basename(sound.split('.')[0])
+            self._data['sounds'].append(Resource(fn,snd))
+
+        # -- load levels
+        for level in os.listdir(self._levels):
+            lvl = pg.resource.file('levels/' + level)
+            fn = os.path.basename(level.split('.')[0])
+            self._data['levels'].append(Resource(fn,lvl))
+
+class SceneManager:
+
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def key_press(self, key, modifiers):
+        pass
+
+    def key_release(self, key, modifiers):
+        pass
+
+    def mouse_press(self, x, y, button, modifiers):
+        pass
+
+    def mouse_release(self, x, y, button, modifiers):
+        pass
+
+    def mouse_motion(self, x, y, dx, dy):
+        pass
+
+
+'''
+============================================================
+---   MAIN
+============================================================
+'''
+
 # -- create window
 window = pg.window.Window(*SIZE, resizable=True)
 window.set_minimum_size(*SIZE)
 window.set_caption(CAPTION)
 
 # -- create manager and resources
-res = Resources()
+res     = Resources()
 manager = SceneManager()
-for scn in init_scenes():
-    manager.add(scn, scn.name == "Main")
 
 background = res.sprite('world_background')
 
