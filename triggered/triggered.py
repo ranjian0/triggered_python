6f1e16dfcd059949a53674218b1038a77f41ae39
ranjian0 <karanjaichungwa@gmail.com>
2018-10-31 23:18:37 +0300

    triggered:: reset blend func

--- triggered/triggered.py
+++ triggered/triggered.py
@@ -2372,7 +2372,7 @@ def on_draw():
     window.clear()
     glClearColor(.39, .39, .39, 1)
     glEnable(GL_BLEND)
-    # glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
     game.draw()
 
     if DEBUG and game.state == GameState.RUNNING:
