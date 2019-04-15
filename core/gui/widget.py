08287836d6f318b2e65a90c1b8af81ca95f3f04a
ranjian0 <karanjaichungwa@gmail.com>
2019-04-15 13:39:49 +0300

    implement update elements, add size methods

--- core/gui/widget.py
+++ core/gui/widget.py
@@ -1,5 +1,5 @@
 import pyglet as pg
-from core.math import Rect
+from core.math import Rect, Size
 
 class Widget(object):
     """Base class for all widgets"""
@@ -18,6 +18,7 @@ class Widget(object):
         # -- size attributes
         self._w = kwargs.get('w', 1)
         self._h = kwargs.get('h', 1)
+        self._pref_size = Size()
 
         # -- layout attributes
         self._rect = Rect(0, 0, 1, 1)
@@ -26,7 +27,7 @@ class Widget(object):
         self._batch = kwargs.get('batch', pg.graphics.Batch())
         self._group = kwargs.get('group', pg.graphics.OrderedGroup(0))
 
-        self._dirty = True
+        self._dirty = False
         self.shapes = dict()
         self.elements = dict()
 
@@ -34,14 +35,14 @@ class Widget(object):
         return self._x
     def _set_x(self, val):
         self._x = val
-        self._dirty = True
+        self.root.update_layout()
     x = property(_get_x, _set_x)
 
     def _get_y(self):
         return self._y
     def _set_y(self, val):
         self._y = val
-        self._dirty = True
+        self.root.update_layout()
     y = property(_get_y, _set_y)
     position = property(lambda self: (self._x, self._y))
 
@@ -49,14 +50,14 @@ class Widget(object):
         return self._w
     def _set_w(self, val):
         self._w = val
-        self._dirty = True
+        self.root.update_layout()
     w = property(_get_w, _set_w)
 
     def _get_h(self):
         return self._h
     def _set_h(self, val):
         self._h = val
-        self._dirty = True
+        self.root.update_layout()
     h = property(_get_h, _set_h)
     size = property(lambda self: (self._w, self._h))
 
@@ -76,54 +77,73 @@ class Widget(object):
     root = property(_find_root)
 
     def update_layout(self):
-        self._dirty = False
+        self._dirty = True
 
     def update_batch(self, batch, group):
+        self._batch, self._group = batch, group
         for idx, shape in enumerate(self.shapes.values()):
             shape.update(batch, pg.graphics.OrderedGroup(idx, group))
 
         for idx, elem in enumerate(self.elements.values()):
             elem.update(batch, pg.graphics.OrderedGroup(idx, group))
 
+        self._dirty = True
+
+    def update_elements(self):
+        self._dirty = False
+
+    def update_global_coords(self):
+        if self.parent:
+            self._gx, self._gy = self.parent._gx + self._x, self.parent._gy + self._y
+        else:
+            self._gx, self._gy = self._x, self._y
+
     def hit_test(self, x, y):
         return self._rect.hit(x, y)
 
+    def determine_size(self):
+        pass
+
+    def reset_size(self, size):
+        self._w, self._h = size
+        self._dirty = True
+
     def on_draw(self):
-        return NotImplementedError()
+        pass
 
     def on_update(self, dt):
-        return NotImplementedError()
+        pass
 
     def on_resize(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_key_press(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_key_release(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_mouse_press(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_mouse_release(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_mouse_drag(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_mouse_motion(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_mouse_scroll(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_text(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_text_motion(self, *args):
-        return NotImplementedError()
+        pass
 
     def on_text_motion_select(self, *args):
-        return NotImplementedError()
+        pass
 
