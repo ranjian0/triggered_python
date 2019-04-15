f703d01d115899df89328bf25e7f3eb5c247b1ef
ranjian0 <karanjaichungwa@gmail.com>
2019-04-15 13:41:05 +0300

    refactor layout, move some functionality to Container

--- core/gui/layout.py
+++ core/gui/layout.py
@@ -1,137 +1,66 @@
 import operator
-from .widget import Widget
-from core.math import Rect
+from core.math import Rect, Size
+from .container import Container
 
 
-class Layout(Widget):
-    """ Base class container for other widgets """
+class Layout(Container):
+    """ Base class container arranging & sizing widgets """
 
     VERTICAL = 1
     HORIZONTAL = 2
-    def __init__(self, *args, **kwargs):
+
+    ALIGN_TOP    = 1
+    ALIGN_LEFT   = 2
+    ALIGN_RIGHT  = 3
+    ALIGN_CENTER = 4
+    ALIGN_BOTTOM = 5
+    def __init__(self, axis, spacing=10, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._children = []
-
-    def _add(self, item):
-        item.parent = self
-        self._children.append(item)
-        self._dirty = True
-
-    def __iadd__(self, item):
-        self._add(item)
-        return self
-
-    def _remove(self, item):
-        item.parent = None
-        self._children.remove(item)
-        self._dirty = True
-
-    def __isub__(self, item):
-        self._remove(item)
-        return self
-
-    def __iter__(self):
-        return iter(self._children)
-
-    def _iter_call_meth(self, method, *args, **kwargs):
-        """ Call meth on this object's __iter__ """
-        for obj in self:
-            if hasattr(obj, method):
-                f = operator.methodcaller(method, *args, **kwargs)
-                f(obj)
-
-    def on_draw(self):
-        self.update_layout()
-        self.batch.draw()
-
-    def on_update(self, dt):
-        self._iter_call_meth('on_update', dt)
-
-    def on_resize(self, *args):
-        self._iter_call_meth('on_resize', *args)
-
-    def on_key_press(self, *args):
-        self._iter_call_meth('on_key_press', *args)
-
-    def on_key_release(self, *args):
-        self._iter_call_meth('on_key_release', *args)
-
-    def on_mouse_press(self, *args):
-        self._iter_call_meth('on_mouse_press', *args)
-
-    def on_mouse_release(self, *args):
-        self._iter_call_meth('on_mouse_release', *args)
-
-    def on_mouse_drag(self, *args):
-        self._iter_call_meth('on_mouse_drag', *args)
-
-    def on_mouse_motion(self, *args):
-        self._iter_call_meth('on_mouse_motion', *args)
-
-    def on_mouse_scroll(self, *args):
-        self._iter_call_meth('on_mouse_scroll', *args)
-
-    def on_text(self, *args):
-        self._iter_call_meth('on_text', *args)
-
-    def on_text_motion(self, *args):
-        self._iter_call_meth('on_text_motion', *args)
-
-    def on_text_motion_select(self, *args):
-        self._iter_call_meth('on_text_motion_select', *args)
-
-
-class BoxLayout(Layout):
-
-    def __init__(self, orient, spacing=5, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._orient = orient
+        self._axis = axis
         self._spacing = spacing
 
-    def update_layout(self):
-        for c in self._children:
-            c.update_layout()
+        #XXX Extra box layout properties
+        # -- make children fill layout
+        self._expand = False
+        # -- anchor children to layout position
+        self._halign = Layout.ALIGN_LEFT
+        self._valign = Layout.ALIGN_TOP
 
-        if self._dirty:
-            self._gx, self._gy = self.root.position
+        # -- add children
+        if args:
+            for item in args:
+                self._add(item)
 
-            w, h = 0, 0
-            if self._orient == Layout.HORIZONTAL:
-                h = max(c.h for c in self._children)
-                w = sum(c.w + self._spacing for c in self._children)
+    def expand(self, val):
+        self._expand = val
 
-                accumulator = 0
-                for c in self._children:
-                    c.y = self._gy
-                    c.x = self._gx + accumulator
-                    c.update_layout()
-                    accumulator += c.w  + self._spacing
+    def align(self, horizontal=None, vertical=None):
+        if horizontal not in ["left", "center", "right"]:
+            raise ValueError('Expected str argument in {"left", "center", "right"}')
+        if vertical not in ["top", "center", "bottom"]:
+            raise ValueError('Expected str argument in {"top", "center", "bottom"}')
 
-            elif self._orient == Layout.VERTICAL:
-                w = max(c.w for c in self._children)
-                h = sum(c.h + self._spacing for c in self._children)
+        if horizontal:
+            self._halign = {"left":   Layout.ALIGN_LEFT,
+                            "right":  Layout.ALIGN_RIGHT,
+                            "center": Layout.ALIGN_CENTER}[horizontal]
+        if vertical:
+            self._valign = {"top":    Layout.ALIGN_TOP,
+                            "center": Layout.ALIGN_CENTER,
+                            "bottom": Layout.ALIGN_BOTTOM}[vertical]
 
-                accumulator = 0
-                for c in self._children:
-                    c.x = self._gx
-                    c.y = self._gy - accumulator
-                    c.update_layout()
-                    accumulator += c.h + self._spacing
+    def determine_size(self):
+        pass
 
-            self._w, self._h = w, h
-            self._rect = Rect(self._gx, self._gy, self._w, self._h)
-            self.update_batch(self._batch, self._group)
-        super().update_layout()
-
-    def update_batch(self, batch, group):
-        for c in self._children:
-            c.update_batch(self._batch, self._group)
+    def reset_size(self, size):
+        super().reset_size(size)
+        pass
 
 
-class HBoxLayout(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(Layout.HORIZONTAL, **kwargs)
+class HBoxLayout(Layout):
+    def __init__(self, *args, **kwargs):
+        super().__init__(Layout.HORIZONTAL, *args, **kwargs)
 
-class VBoxLayout(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(Layout.VERTICAL, **kwargs)
+class VBoxLayout(Layout):
+    def __init__(self, *args, **kwargs):
+        super().__init__(Layout.VERTICAL, *args, **kwargs)
