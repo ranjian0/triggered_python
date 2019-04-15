e9091dc826b8cf1a96b9e642b48e528f60734e86
ranjian0 <karanjaichungwa@gmail.com>
2019-04-15 13:37:06 +0300

    implement point, size and rect classes

--- core/math.py
+++ core/math.py
@@ -27,22 +27,95 @@ class Vec2(pm.vec2d.Vec2d):
     def to_tuple(self, precision=4):
         return round(float(self.x), precision), round((self.y), precision)
 
-class Rect(object):
-    def __init__(self, x, y, w, h):
-        super().__init__()
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
+class Point(object):
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.x, self.y = args[0], args[1]
+        elif len(args) == 1:
+            self.x, self.y = args[0][0], args[0][1]
+        else:
+            self.x, self.y = 0, 0
 
-    size = property(lambda self: (self.w, self.h))
-    position = property(lambda self: (self.x, self.y))
+    def __getitem__(self, i):
+        return (self.x, self.y)[i]
+
+    def __iter__(self):
+        def gen():
+            for i in (self.x, self.y):
+                yield i
+        return gen()
+
+    def __add__(self, other):
+        return Point(self.x + other[0], self.y + other[1])
+
+class Size(object):
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.w, self.h = args[0], args[1]
+        elif len(args) == 1:
+            self.w, self.h = args[0][0], args[0][1]
+        else:
+            self.w, self.h = 0, 0
+
+    def __getitem__(self, i):
+        return (self.w, self.h)[i]
+
+    def __iter__(self):
+        def gen():
+            for i in (self.w, self.h):
+                yield i
+        return gen()
+
+    def hit_test(self, x, y):
+        return (x >= 0 and x <= self.w) and (y >= 0 and y <= self.h)
+
+    def __add__(self, other):
+        return Size(self.w + other[0], self.h + other[1])
+
+class Rect(object):
+    '''Fast and simple rectangular collision structure'''
+
+    def __init__(self, x=0, y=0, w=0, h=0):
+        '''Create a rectangle'''
+        self.x, self.y = x, y
+        self.w, self.h = w, h
+
+    def copy(self):
+        return Rect(self.x, self.y, self.w, self.h)
+
+    def intersect(self, r):
+        '''Compute the intersection of two rectangles'''
+        if not self.collides(r):
+            return Rect(0, 0, 0, 0)
+        x, y = max(self.x, r.x), max(self.y, r.y)
+        x2, y2 = min(self.x+self.w, r.x+r.w), min(self.y+self.h, r.y+r.h)
+        n = Rect( x, y, x2 - x, y2 - y )
+        return n
+
+    def collides(self, r):
+        '''Determine whether two rectangles collide'''
+        if self.x+self.w < r.x or self.y+self.h < r.y or \
+                self.x > r.x + r.w or self.y > r.y + r.h:
+            return False
+        return True
+
+    def hit_test(self, x, y):
+        '''Determine whether a point is inside the rectangle'''
+        return (x >= self.x and x <= self.x + self.w) and (y >= self.y and y <= self.y + self.h)
+
+    @property
+    def min(self):
+        return (self.x, self.y)
+
+    @property
+    def max(self):
+        return (self.x + self.w, self.y + self.h)
+
+    def __iter__(self):
+        return iter((self.x, self.y, self.w, self.h))
 
     def __repr__(self):
-        return f"Rect({self.x}, {self.y}, {self.w}, {self.h})"
-
-    def hit(self, x, y):
-        return (self.x <= x <= self.w) and (self.y <= y <= self.h)
+        return 'Rect(%d %d %d %d)' % (self.x, self.y, self.w, self.h)
 
 
 class Bounds(namedtuple('_Bounds', "left bottom right top")):
