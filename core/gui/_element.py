5fc5be9f6a198efa4b405b7ce77bfa18566f186d
ranjian0 <karanjaichungwa@gmail.com>
2019-04-15 14:49:49 +0300

    update text elements update to update_batch

--- core/gui/_element.py
+++ core/gui/_element.py
@@ -6,6 +6,11 @@ class TextElement(pg.text.Label):
         self.anchor_y = "top"
         self.anchor_x = "left"
 
-    def update(self, batch, group):
+    def update_batch(self, batch, group):
         self.batch = batch
-        self.foreground_group = group
+
+        self.top_group = pg.text.layout.TextLayoutGroup(group)
+        self.background_group = pg.graphics.OrderedGroup(0, self.top_group)
+        self.foreground_group = pg.text.layout.TextLayoutForegroundGroup(1, self.top_group)
+        self.foreground_decoration_group = pg.text.layout.TextLayoutForegroundDecorationGroup(2, self.top_group)
+        self._update()
