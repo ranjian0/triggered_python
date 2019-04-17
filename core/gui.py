f8eebc48ad1828c78f17c112fe9861b4d49b40c0
ranjian0 <karanjaichungwa@gmail.com>
2019-04-17 18:40:11 +0300

    implement base widget class

--- core/gui.py
+++ core/gui.py
@@ -15,3 +15,99 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 
+class Widget(object):
+    """Base class for all widgets"""
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        # -- position attributes
+        self._x = kwargs.get('x', 0)
+        self._y = kwargs.get('y', 0)
+
+        # -- size attributes
+        self._w = kwargs.get('w', 1)
+        self._h = kwargs.get('h', 1)
+
+        # -- layout attributes
+        self._rect = Rect(0, 0, 1, 1)
+
+        # -- draw attributes
+        self._batch = kwargs.get('batch', pg.graphics.Batch())
+        self._group = kwargs.get('group', pg.graphics.OrderedGroup(0))
+
+        self._dirty = False
+        self.shapes = dict()
+        self.elements = dict()
+
+    def _get_x(self):
+        return self._x
+    def _set_x(self, val):
+        self._x = val
+    x = property(_get_x, _set_x)
+
+    def _get_y(self):
+        return self._y
+    def _set_y(self, val):
+        self._y = val
+    y = property(_get_y, _set_y)
+    position = property(lambda self: (self._x, self._y))
+
+    def _get_w(self):
+        return self._w
+    def _set_w(self, val):
+        self._w = val
+    w = property(_get_w, _set_w)
+
+    def _get_h(self):
+        return self._h
+    def _set_h(self, val):
+        self._h = val
+    h = property(_get_h, _set_h)
+    size = property(lambda self: (self._w, self._h))
+
+    def _get_batch(self):
+        return self._batch
+    batch = property(_get_batch)
+
+    def _get_group(self):
+        return self._group
+    group = property(_get_group)
+
+    def on_draw(self):
+        pass
+
+    def on_update(self, dt):
+        pass
+
+    def on_resize(self, *args):
+        pass
+
+    def on_key_press(self, *args):
+        pass
+
+    def on_key_release(self, *args):
+        pass
+
+    def on_mouse_press(self, *args):
+        pass
+
+    def on_mouse_release(self, *args):
+        pass
+
+    def on_mouse_drag(self, *args):
+        pass
+
+    def on_mouse_motion(self, *args):
+        pass
+
+    def on_mouse_scroll(self, *args):
+        pass
+
+    def on_text(self, *args):
+        pass
+
+    def on_text_motion(self, *args):
+        pass
+
+    def on_text_motion_select(self, *args):
+        pass
+
